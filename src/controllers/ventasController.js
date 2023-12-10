53415816f3bdeecab917d1959a083d83ad959f59
Ikaedso <juanfelipesanchezsanchezherrer@gmail.com>
2023-12-10 16:30:52 -0500

    para hacer el nuevo pull

--- src/controllers/ventasController.js
+++ src/controllers/ventasController.js
@@ -12,10 +12,17 @@ const getVentasPage = async (req, res) => {
     const ventasCollection = database.collection('ventas');
 
     // Realizar operaciones con la colección de ventas...
-    const ventas = await ventasCollection.find({}).toArray(); // Ejemplo: Obtener todos los documentos de ventas
+    const ventas = await ventasCollection.find({}).toArray(); // Obtener todos los documentos de ventas
 
-    // Renderizar la vista con datos de ventas
-    res.render('ventas', { ventas });
+    // Obtener los pedidos y usuarios desde sus respectivas colecciones
+    const pedidosCollection = database.collection('pedidos');
+    const usuariosCollection = database.collection('configuracion');
+
+    const pedidos = await pedidosCollection.find({}).toArray(); // Obtener todos los documentos de pedidos
+    const usuarios = await usuariosCollection.find({}).toArray(); // Obtener todos los usuarios
+
+    // Renderizar la vista con datos de ventas, pedidos y usuarios
+    res.render('ventas', { ventas, pedidos, usuarios });
   } catch (error) {
     console.error('Error al obtener datos de la base de datos:', error);
     res.status(500).send('Error interno del servidor');
@@ -24,6 +31,8 @@ const getVentasPage = async (req, res) => {
   }
 };
 
+module.exports = { getVentasPage };
+
 const agregarVenta = async (req, res) => {
   const uri = 'mongodb+srv://jhomai7020:1097183614@sena.kpooaa3.mongodb.net/erikas_homemade';
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -31,10 +40,15 @@ const agregarVenta = async (req, res) => {
   try {
     await client.connect(); // Conectar a la base de datos
 
-    const database = client.db('erikas_homemade'); // Usar el nombre correcto de la base de datos
+    const database = client.db('erikas_homemade');
     const ventasCollection = database.collection('ventas');
+    const pedidosCollection = database.collection('pedidos');
+    const configuracionCollection = database.collection('configuracion');
 
-    // Obtener los datos de la nueva venta desde la solicitud (req)
+    // Obtener datos de pedidos y configuracion
+    const pedidos = await pedidosCollection.find({}, { projection: { servicios: 1, productos: 1 } }).toArray();
+    const usuarios = await configuracionCollection.find({}).toArray();
+
     const { fecha_venta, metodo_pago, total_venta, total_pedido, nombre, telefono, documento, correo, productos } = req.body;
 
     // Construir el objeto de venta
@@ -50,17 +64,11 @@ const agregarVenta = async (req, res) => {
       productos
       // Agregar otros campos de la venta según sea necesario
     };
-
-    // Agregar la nueva venta a la colección de ventas en la base de datos
-    await ventasCollection.insertOne(nuevaVenta);
-    
-    const ventas = await ventasCollection.find({}).toArray();
-
-    // Redireccionar a la página de ventas después de agregar la venta
-    res.render('ventas', { ventas }); // Reemplazar con la ruta correcta
+    // Renderizar la vista de ventas con los datos necesarios para el formulario
+    res.render('ventas', { ventas, pedidos, usuarios });
   } catch (error) {
-    console.error('Error al agregar la venta:', error);
-    res.status(500).send('Error interno del servidor al agregar la venta');
+    console.error('Error al cargar datos para la venta:', error);
+    res.status(500).send('Error interno del servidor al cargar datos para la venta');
   } finally {
     await client.close(); // Cerrar la conexión
   }
